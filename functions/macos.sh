a8d853e91c545cb8dee806417a5a6bdd5910f2c6
Carlos Escriche <escrichov@gmail.com>
2018-03-27 00:39:27 +0200

    Backup-conf and restore-conf as alias

--- functions/macos.sh
+++ functions/macos.sh
@@ -10,3 +10,6 @@ alias dns-flush='sudo killall -HUP mDNSResponder'
 
 # Scan wifi networks with: "airport -s"
 alias airport=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport
+
+alias backup-conf="mackup backup -f"
+alias restore-conf="mackup restore -f"
