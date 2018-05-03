cf5661d2a77cb1b22d24bf70ffcd45f5ed139f6a
Carlos Escriche <escrichov@gmail.com>
2018-05-03 19:41:37 +0200

    Fixed bug installing nokogiri gem

--- install/ruby/gems.sh
+++ install/ruby/gems.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+brew link --overwrite ruby && reload
 
+gem install nokogiri -- --use-system-libraries=true --with-xml2-include=$(brew --prefix libxml2)/include/libxml2
 gem install jekyll
 gem install bundler
