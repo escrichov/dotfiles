f8c32fd52c6ef5ed8169f3a31f4baf58a5e54209
Carlos Escriche <escrichov@gmail.com>
2018-10-04 00:42:54 +0200

    Added tmuxinator, magnet and visual studio code. Disable spectacle

--- install/ruby/gems.sh
+++ install/ruby/gems.sh
@@ -5,3 +5,4 @@ brew link --overwrite ruby && reload
 gem install nokogiri -- --use-system-libraries=true --with-xml2-include=$(brew --prefix libxml2)/include/libxml2
 gem install jekyll
 gem install bundler
+gem install tmuxinator
