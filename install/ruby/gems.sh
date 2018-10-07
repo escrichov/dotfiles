26bcb3cd3826cc573750110875b524678b467091
Carlos Escriche <escrichov@gmail.com>
2018-10-07 20:54:12 +0200

    Remove ruby installation warning

--- install/ruby/gems.sh
+++ install/ruby/gems.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-brew link --overwrite ruby && reload
+brew unlink ruby && brew link ruby && reload
 
 gem install nokogiri -- --use-system-libraries=true --with-xml2-include=$(brew --prefix libxml2)/include/libxml2
 gem install jekyll
