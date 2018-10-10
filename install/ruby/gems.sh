1b450cfe1d88e5727c980387c55b86ad8e374b18
Carlos Escriche <escrichov@gmail.com>
2018-10-10 23:45:30 +0200

    Remove reload from ruby gems scripts

--- install/ruby/gems.sh
+++ install/ruby/gems.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-brew unlink ruby && brew link ruby && reload
+brew unlink ruby && brew link ruby
 
 gem install nokogiri -- --use-system-libraries=true --with-xml2-include=$(brew --prefix libxml2)/include/libxml2
 gem install jekyll
