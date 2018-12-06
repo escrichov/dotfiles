c8036b05cf7c2dd1ddd313f63d7ce13557376766
Carlos Escriche <escrichov@gmail.com>
2018-12-06 16:45:43 +0100

    Check and activate venv

--- functions/python.sh
+++ functions/python.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+export PIPENV_VENV_IN_PROJECT=1
 
 gpip2(){
     PIP_REQUIRE_VIRTUALENV="" pip2 "$@"
@@ -17,21 +18,40 @@ alias pyc-remove='find . -name "*.pyc" -exec rm -f {} \;'
 
 function activate_virtualenv {
 
+	function check_and_activate_pipenv {
+		if [ -f Pipfile ]; then
+			pipenv shell
+			return 0
+		fi
+
+		return 1
+	}
+
 	function check_and_activate {
 		V_DIR=$1
 
 		if [ -d $V_DIR ]; then
 			if [ -f $V_DIR/bin/activate ]; then
 				source $V_DIR/bin/activate
-				return 1
+				return 0
 			fi
 		fi
-		return 0
+		return 1
 	}
+
+	check_and_activate_pipenv
+	if [ $? = 0 ]; then
+		return 0
+	fi
 
 	check_and_activate 'env'
 	if [ $? = 0 ]; then
-		check_and_activate '.env'
+		return 0
+	fi
+	
+	check_and_activate '.env'
+	if [ $? = 0 ]; then
+		return 0
 	fi
 }
 
