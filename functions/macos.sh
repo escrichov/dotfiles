22845cbe8cb0cee26a94a8e0e358fd3ea7e9f61e
Carlos Escriche Izquierdo <escrichov@gmail.com>
2021-11-27 20:19:29 +0100

    Commented rmtrash package

--- functions/macos.sh
+++ functions/macos.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-
-# No use rm
-alias rm="echo Use 'del', or the full path i.e. '/bin/rm'"
-alias trash="rmtrash"
-alias del="rmtrash"
-
 alias dns-flush='sudo killall -HUP mDNSResponder'
 
 # Scan wifi networks with: "airport -s"
