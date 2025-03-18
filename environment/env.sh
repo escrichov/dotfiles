6fba659533c80100deba1711cce0cddcb17cc8ab
Carlos Escriche Izquierdo <escrichov@gmail.com>
2025-03-18 11:37:05 +0100

    Added nvm dir

--- environment/env.sh
+++ environment/env.sh
@@ -63,6 +63,7 @@ export PAGER="less -R"
 export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+export PATH=$PATH:$NVM_DIR
 
 # pyenv
 export PYENV_ROOT="$HOME/.pyenv"
